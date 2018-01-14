ab52370176387b5d8b5775ce30b7f827996dae92
vibakar <vibakar23@gmail.com>
2018-01-14 16:04:39 +0530

    uploading profile pic button disabled for now

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('*', (req, res) => {
 });
 
 //Get port from environment and store in Express
-const port = process.env.PORT || '8079';
+const port = process.env.PORT || '8070';
 app.set('port', port);
 
 // Create HTTP server.
